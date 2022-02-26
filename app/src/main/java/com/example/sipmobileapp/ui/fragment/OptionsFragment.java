b8c06f732e567f01a2d040ce23bcaa5fcea78d67
Arezoo <arezoo.sdghi@gmail.com>
2022-02-26 15:27:19 +0330

    Making design better.

--- app/src/main/java/com/example/sipmobileapp/ui/fragment/OptionsFragment.java
+++ app/src/main/java/com/example/sipmobileapp/ui/fragment/OptionsFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,13 +17,17 @@ import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.sipmobileapp.R;
-import com.example.sipmobileapp.databinding.FragmentOptionsBinding;
+import com.example.sipmobileapp.databinding.BaseLayoutBinding;
+import com.example.sipmobileapp.model.ServerData;
 import com.example.sipmobileapp.ui.dialog.SignOutQuestionDialogFragment;
 import com.example.sipmobileapp.utils.SipMobileAppPreferences;
 import com.example.sipmobileapp.viewmodel.OptionsViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OptionsFragment extends Fragment {
-    private FragmentOptionsBinding binding;
+    private BaseLayoutBinding binding;
     private OptionsViewModel viewModel;
     private int topBarIconSize, bottomBarIconSize, iconSize;
 
@@ -45,45 +50,84 @@ public class OptionsFragment extends Fragment {
 
         binding = DataBindingUtil.inflate(
                 inflater,
-                R.layout.fragment_options,
+                R.layout.base_layout,
                 container,
                 false);
 
         handleEvents();
 
-        int orientation = getResources().getConfiguration().orientation;
+        int orientation = getOrientation();
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            topBarIconSize = getScreenWidth() / 18;
-            bottomBarIconSize = getScreenWidth() / 24;
-            iconSize = getScreenWidth() / 6;
+            int screenWidth = getScreenWidth();
+            topBarIconSize = screenWidth / 18;
+            bottomBarIconSize = screenWidth / 24;
+            iconSize = screenWidth / 6;
+
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) binding.btnClock.getLayoutParams();
+            layoutParams.width = screenWidth / 9;
+            layoutParams.height = screenWidth / 9;
+            binding.btnClock.setLayoutParams(layoutParams);
+
         } else {
-            topBarIconSize = getScreenWidth() / 27;
-            bottomBarIconSize = getScreenWidth() / 30;
-            iconSize = getScreenWidth() / 10;
+            int screenHeight = getScreenHeight();
+            topBarIconSize = screenHeight / 18;
+            bottomBarIconSize = screenHeight / 20;
+            iconSize = screenHeight / 6;
+
+            int height = screenHeight / 9;
+            ViewGroup.LayoutParams layoutParamsContainerOne = binding.containerOne.getLayoutParams();
+            layoutParamsContainerOne.height = height;
+            binding.containerOne.setLayoutParams(layoutParamsContainerOne);
+
+            ViewGroup.LayoutParams layoutParamsContainerThree = binding.containerThree.getLayoutParams();
+            layoutParamsContainerThree.height = height;
+            binding.containerThree.setLayoutParams(layoutParamsContainerThree);
+
+            ViewGroup.LayoutParams layoutParamsContainerTwo = binding.spinner.getLayoutParams();
+
+            float totalSize = (2 * height + (layoutParamsContainerTwo.height) + 20);
+            float remainingSize = screenHeight - totalSize;
+
+            ViewGroup.LayoutParams layoutParamsInclude = binding.includeOptions.containerMain.getLayoutParams();
+            layoutParamsInclude.height = (int) (remainingSize);
+            binding.includeOptions.containerMain.setLayoutParams(layoutParamsInclude);
+
+            ViewGroup.LayoutParams layoutParamsInnerContainerOne = binding.includeOptions.innerContainerOne.getLayoutParams();
+            layoutParamsInnerContainerOne.height = (int) ((remainingSize) / 2);
+            binding.includeOptions.innerContainerOne.setLayoutParams(layoutParamsInnerContainerOne);
+
+            ViewGroup.LayoutParams layoutParamsInnerContainerTwo = binding.includeOptions.innerContainerTwo.getLayoutParams();
+            layoutParamsInnerContainerTwo.height = (int) ((remainingSize) / 2);
+            binding.includeOptions.innerContainerTwo.setLayoutParams(layoutParamsInnerContainerTwo);
+
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) binding.btnClock.getLayoutParams();
+            layoutParams.width = height;
+            layoutParams.height = height;
+            binding.btnClock.setLayoutParams(layoutParams);
         }
 
-        binding.btnSetting.setIconSize(topBarIconSize);
-        binding.btnTalk.setIconSize(topBarIconSize);
-        binding.btnPhone.setIconSize(topBarIconSize);
-        binding.btnHelp.setIconSize(topBarIconSize);
-        binding.btnCoin.setIconSize(topBarIconSize);
-        binding.btnColor.setIconSize(topBarIconSize);
-        binding.btnLock.setIconSize(topBarIconSize);
-        binding.btnFile.setIconSize(topBarIconSize);
         binding.btnClose.setIconSize(topBarIconSize);
+        binding.btnFile.setIconSize(topBarIconSize);
+        binding.btnLock.setIconSize(topBarIconSize);
+        binding.btnColor.setIconSize(topBarIconSize);
+        binding.btnCoin.setIconSize(topBarIconSize);
+        binding.btnHelp.setIconSize(topBarIconSize);
+        binding.btnPhone.setIconSize(topBarIconSize);
+        binding.btnTalk.setIconSize(topBarIconSize);
+        binding.btnSetting.setIconSize(topBarIconSize);
 
         binding.btnUser.setIconSize(bottomBarIconSize);
-        binding.ivClock.setIconSize(bottomBarIconSize);
+        binding.btnClock.setIconSize(bottomBarIconSize);
 
-        binding.btnSecretory.setIconSize(iconSize);
-        binding.btnDoctor.setIconSize(iconSize);
-        binding.btnProfessional.setIconSize(iconSize);
-        binding.btnInsuranceList.setIconSize(iconSize);
-        binding.btnChangePassword.setIconSize(iconSize);
-        binding.btnAnyDesk.setIconSize(iconSize);
-        binding.btnPhoneBook.setIconSize(iconSize);
-        binding.btnStore.setIconSize(iconSize);
-        binding.btnDownload.setIconSize(iconSize);
+        binding.includeOptions.btnSecretory.setIconSize(iconSize);
+        binding.includeOptions.btnDoctor.setIconSize(iconSize);
+        binding.includeOptions.btnProfessional.setIconSize(iconSize);
+        binding.includeOptions.btnInsuranceList.setIconSize(iconSize);
+        binding.includeOptions.btnChangePassword.setIconSize(iconSize);
+        binding.includeOptions.btnAnyDesk.setIconSize(iconSize);
+        binding.includeOptions.btnPhoneBook.setIconSize(iconSize);
+        binding.includeOptions.btnStore.setIconSize(iconSize);
+        binding.includeOptions.btnDownload.setIconSize(iconSize);
 
         return binding.getRoot();
     }
@@ -96,6 +140,14 @@ public class OptionsFragment extends Fragment {
 
     private void createViewModel() {
         viewModel = new ViewModelProvider(requireActivity()).get(OptionsViewModel.class);
+    }
+
+    private void setupSpinner(List<ServerData> serverDataList) {
+        List<String> centerNames = new ArrayList<>();
+        for (ServerData serverData : serverDataList) {
+            centerNames.add(serverData.getCenterName());
+        }
+        binding.spinner.setItems(centerNames);
     }
 
     private void handleEvents() {
@@ -111,11 +163,25 @@ public class OptionsFragment extends Fragment {
         return displayMetrics.widthPixels;
     }
 
+    private int getScreenHeight() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+
+    private int getOrientation() {
+        return getResources().getConfiguration().orientation;
+    }
+
     private void setupObserver() {
         viewModel.getYesSignOutClicked().observe(getViewLifecycleOwner(), yesSignOutClicked -> {
             SipMobileAppPreferences.setUserLoginKey(getContext(), null);
             NavDirections action = OptionsFragmentDirections.actionOptionsFragmentToLoginFragment();
             NavHostFragment.findNavController(this).navigate(action);
+        });
+
+        viewModel.getServerDataListMutableLiveData().observe(getViewLifecycleOwner(), serverDataList -> {
+            setupSpinner(serverDataList);
         });
     }
 }
--- app/src/main/java/com/example/sipmobileapp/viewmodel/OptionsViewModel.java
+++ app/src/main/java/com/example/sipmobileapp/viewmodel/OptionsViewModel.java
@@ -4,12 +4,26 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.sipmobileapp.model.ServerData;
+import com.example.sipmobileapp.repository.SipMobileAppRepository;
+
+import java.util.List;
 
 public class OptionsViewModel extends AndroidViewModel {
+    private SipMobileAppRepository repository;
+    private LiveData<List<ServerData>> serverDataListMutableLiveData;
     private SingleLiveEvent<Boolean> yesSignOutClicked = new SingleLiveEvent<>();
 
     public OptionsViewModel(@NonNull Application application) {
         super(application);
+        repository = SipMobileAppRepository.getInstance(getApplication());
+        serverDataListMutableLiveData = repository.getServerDataListMutableLiveData();
+    }
+
+    public LiveData<List<ServerData>> getServerDataListMutableLiveData() {
+        return serverDataListMutableLiveData;
     }
 
     public SingleLiveEvent<Boolean> getYesSignOutClicked() {
